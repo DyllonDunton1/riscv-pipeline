ee3ec9fadf4faaf44f4261e9212b4c4520660eb3
Dyllon Dunton <duntondyllon@gmail.com>
2023-10-25 16:47:17 -0400

    Milestone 1 Complete

--- controller.v
+++ controller.v
@@ -9,11 +9,12 @@ module controller (
 	always @ (posedge clock) begin
 		case (instruction_type)
 			5'b100000	:	ALUSRC = 0; //R
-			5'b010000	:	ALUSRC = 0; //I
+			5'b010000	:	ALUSRC = 1; //I
 			5'b001000	:	ALUSRC = 0; //S
 			5'b000100	:	ALUSRC = 0; //B
 			5'b000010	:	ALUSRC = 0; //J
 			5'b000001	:	ALUSRC = 0; //U
+			default	:	ALUSRC = 0;
 		endcase	
 	end	
 	
--- id_ex.v
+++ id_ex.v
@@ -2,17 +2,23 @@ module id_ex (
 	input wire [31:0] data_in_1,
 	input wire [31:0] data_in_2,
 	input wire [4:0] rd_in,
+	input wire [31:0] imm_in,
+	input wire alusrc_in,
 	input wire clock,
 	
 	output reg [31:0] data_out_1,
 	output reg [31:0] data_out_2,
-	output reg [4:0] rd_out
+	output reg [4:0] rd_out,
+	output reg [31:0] imm_out,
+	output reg alusrc_out
 );
 	
 	always @ (posedge clock) begin
 		data_out_1 = data_in_1;
 		data_out_2 = data_in_2;
 		rd_out = rd_in;
+		imm_out = imm_in;
+		alusrc_out = alusrc_in;
 	end	
 	
 
