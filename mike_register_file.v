77d5bae724f284e73853c8c3fcc806a28c414b0f
Mike Schmitt <michael.schmitt@maine.edu>
2023-11-14 18:33:40 -0500

    switched to register file that uses arrays

--- mike_register_file.v
+++ mike_register_file.v
@@ -1,6 +1,6 @@
-// file register_file.v
+// file mike_register_file.v
 
-module register_file(
+module mike_register_file(
 	input wire [4:0] read_address_1,
 	input wire [4:0] read_address_2,
 	input wire [31:0] write_data_in,
@@ -27,16 +27,18 @@ module register_file(
 	end
 
 	always @(posedge clock or posedge reset) begin
-		/* set the data_out based on read_address */
-		data_out_1 <= r[read_address_1];
-		data_out_2 <= r[read_address_2];
-
-		if (reset == 1'b1) begin
+		if (reset) begin
 			for (i = 0; i < 32; i = i + 1) begin
 				r[i] <= i;
 			end
-		end else if ((write_enable == 1'b1) && write_address != 0) begin
-			r[write_address] <= write_data_in;
+		end else begin
+			/* set the data_out based on read_address */
+			data_out_1 <= r[read_address_1];
+			data_out_2 <= r[read_address_2];
+
+			if ((write_enable == 1'b1) && write_address != 0) begin
+				r[write_address] <= write_data_in;
+			end
 		end
 
 	end
