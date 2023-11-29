b7ab5fd2ec2aa186bf5b54eade97645bb878d73f
Dyllon Dunton <duntondyllon@gmail.com>
2023-11-29 14:40:51 -0500

    Add branch operation control

--- branch_compare.v
+++ branch_compare.v
@@ -1,5 +1,5 @@
 module branch_compare ( 
-	input wire br,
+	input wire [2:0] br,
 	input wire reset,
 	input wire [31:0] rs1,
 	input wire [31:0] rs2,
@@ -17,9 +17,33 @@ module branch_compare (
 			new_address = 1;
 			success = 0;
 		end else begin
-			if (br) begin //IF B-TYPE INSTRUCTION
+			if (br != 0) begin //IF B-TYPE INSTRUCTION
 				success = 0;
-				if ((rs2 - rs1) == 32'd0) begin //IF BEQ
+				if (((rs2 - rs1) == 32'd0) && br == 3'd1) begin //IF BEQ
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (((rs2 - rs1) != 32'd0) && br == 3'd2) begin //IF BNE
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (((rs2 - rs1) < 32'd0) && br == 3'd3) begin //IF BLT
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (((rs2 - rs1) >= 32'd0) && br == 3'd4) begin //IF BGE
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (((rs2 - rs1) < 32'd0) && br == 3'd5) begin //IF BLTU
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (((rs2 - rs1) >= 32'd0) && br == 3'd6) begin //IF BGEU
+					new_address = (imm>>1);
+					success = 1;
+				end
+				if (br == 3'd7) begin //IF JAL
 					new_address = (imm>>1);
 					success = 1;
 				end
