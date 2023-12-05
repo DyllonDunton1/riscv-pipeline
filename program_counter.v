897dfda1c5b778a7e5bcbf5c46cdb10bf607af61
Dyllon Dunton <duntondyllon@gmail.com>
2023-12-04 20:42:17 -0500

    Make PC 4bit

--- program_counter.v
+++ program_counter.v
@@ -10,18 +10,18 @@ module program_counter (
 );
 	
 	initial begin
-		current = -1;
+		current = 8'h00400000 - 8'h4;
 	end
 	
 	always @ (posedge clock or posedge reset) begin
 		if (reset == 1'b1) begin
-			current = 6'd63;
+			current = 8'h00400000 - 8'h4;
 		end else begin
 			if (succ == 1) begin
 				current = new_addr;
 			end
 			else begin
-				current = previous + 1;
+				current = previous + 4;
 			end	
 		end
 	end	
